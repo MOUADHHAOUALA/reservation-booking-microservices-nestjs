634861b2df3c421e63640b516be35c538e49bf8c
MOUADHHAOUALA <mouad-fe@outlook.com>
2025-07-01 19:56:32 +0100

    creating env file setup mongodb url and installing joi validation

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/bunny')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (ConfigService: ConfigService) => ({
+        uri: ConfigService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
