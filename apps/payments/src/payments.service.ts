03f2fea2afdcbe621ad47153c1390997b9fe75a2
MOUADHHAOUALA <mouad-fe@outlook.com>
2025-07-06 19:23:08 +0100

    full stripe payment setup for reservation server

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,37 +1,37 @@
+import { CreateChargeDto } from '@app/common';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe: Stripe;
 
-  constructor(private readonly configService: ConfigService) {
-    const stripeSecretKey = this.configService.get<string>('STRIPE_SECRET_KEY');
-    if (!stripeSecretKey) {
-      throw new Error(
-        'STRIPE_SECRET_KEY is not defined in environment variables',
-      );
-    }
-    this.stripe = new Stripe(stripeSecretKey, {
-      apiVersion: '2025-06-30.basil',
-    });
+  constructor(
+    private readonly configService: ConfigService<{
+      STRIPE_SECRET_KEY: string;
+    }>,
+  ) {
+    this.stripe = new Stripe(
+      this.configService.getOrThrow<string>('STRIPE_SECRET_KEY'),
+      {
+        apiVersion: '2025-06-30.basil',
+      },
+    );
   }
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
+  async createCharge({ amount }: CreateChargeDto) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: amount * 100, // Convert to cents
+      confirm: true,
+      currency: 'usd',
+      payment_method: 'pm_card_visa',
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never',
+      },
     });
 
-    const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
-      amount: amount * 100,
-      confirm: true,
-      payment_method_types: ['card'],
-      currency: 'usd',
-    });
     return paymentIntent;
   }
 }
