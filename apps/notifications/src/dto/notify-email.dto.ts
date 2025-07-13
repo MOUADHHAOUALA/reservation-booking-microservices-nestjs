fc3d631eb707693e39b4102bc6f7132315e9bd01
MOUADHHAOUALA <mouad-fe@outlook.com>
2025-07-13 21:12:37 +0100

    new update is made

--- apps/notifications/src/dto/notify-email.dto.ts
+++ apps/notifications/src/dto/notify-email.dto.ts
@@ -1,6 +1,20 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class NotifyEmailDto {
-  @IsEmail()
+  @IsEmail(
+    {},
+    {
+      message: 'Please provide a valid email address',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email address cannot be empty',
+  })
+  @IsString({
+    message: 'Email must be a string',
+  })
+  @MaxLength(254, {
+    message: 'Email address cannot be longer than 254 characters',
+  })
   email: string;
 }
